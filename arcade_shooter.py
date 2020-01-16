30ec2eb2a384780360a18bbed6d039cf04fc6f1f
myrmica-habilis <michalportes1@gmail.com>
2020-01-16 19:43:30 +0100

    Add scheduled methods

--- arcade_shooter.py
+++ arcade_shooter.py
@@ -1,3 +1,5 @@
+import random
+
 import arcade
 
 SCREEN_WIDTH = 600
@@ -23,6 +25,20 @@ class SpaceShooter(arcade.Window):
         self.player.left = 10
 
         self.all_sprites.append(self.player)
+
+        arcade.schedule(self.add_enemy, 0.25)
+        arcade.schedule(self.add_cloud, 1.0)
+
+    def add_enemy(self, delta_time: float):
+        enemy = arcade.Sprite('images/missile.png', SCALING)
+        enemy.left = random.randint(self.width, self.width + 80)
+        enemy.top = random.randint(10, self.height - 10)
+        enemy.velocity = (random.randint(-20, -5), 0)
+
+        self.enemies_list.append(enemy)
+        self.all_sprites.append(enemy)
+
+    def add_cloud(self, delta_time: float): ...
 
     def on_draw(self):
         arcade.start_render()
